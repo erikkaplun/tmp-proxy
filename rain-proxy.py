b0b3ae484e9b42f2550775e1b574244c9b5c4181
Erik Allik <erik.allik@skype.net>
2012-04-20 22:12:30 +0300

    Grouped IP and port pairs into tuples

--- rain-proxy.py
+++ rain-proxy.py
@@ -52,11 +52,9 @@ def close_socket(connection, local_to_server, sockets):
     sockets.remove(connection)
 
 
-def proxy(remote_ip, remote_port, local_ip, local_port, size=1024, local_to_server_func=None, server_to_local_func=None):
+def proxy(remote_addr, local_addr, size=1024, local_to_server_func=None, server_to_local_func=None):
     local_to_server = {}
     server_to_local = {}
-    remote_addr = (remote_ip, remote_port)
-    local_addr = (local_ip, local_port)
     sockets = []
 
     data_to_write = {}
@@ -119,7 +117,7 @@ def to_client(data):
 if __name__ == '__main__':
     try:
         #~ proxy.proxy('212.95.32.219', 8005, '0.0.0.0', 8005, 8192,to_server, to_client)
-        proxy('195.50.209.244', 80, '0.0.0.0', 80, 1024, to_server, to_client)
+        proxy(('195.50.209.244', 80), ('0.0.0.0', 80), 1024, to_server, to_client)
     except KeyboardInterrupt:
         sys.exit(1)
     else:
